2773bfaccadbf7e52baed082c587130cff3c5a37
Brendan Threewitt <brendanthreewitt@gmail.com>
2022-10-14 04:26:00 -0600

    Update coordinatePointer.js

--- coordinatePointer.js
+++ coordinatePointer.js
@@ -11,7 +11,7 @@ function coordinatePointer(){
     if(mouseY < 22) {rectY += 31; textY += 31;}
     if(mouseX > width - 70) {rectX -= 80; textX -= 80;}
 
-        cursor('crossHair.cur');
+        cursor('CoordinateHelper/crossHair.cur');
         fill(0);
         rect(rectX, rectY, rectWidth, 15, 5);
         fill(255);
