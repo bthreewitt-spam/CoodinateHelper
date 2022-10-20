17710b0f73bd97eddf5dfa0ed3be58074a3c53ec
Brendan <brendanthreewitt@gmail.com>
2022-10-19 22:05:23 -0600

    - added a pinch of transparency to rect - fixed formatting - added noStroke - fixed text size bug

--- CoordinateHelper/coordinatePointer.js
+++ CoordinateHelper/coordinatePointer.js
@@ -1,22 +1,24 @@
 function coordinatePointer(){
     if(mouseX >= 0 && mouseY >= 0 && mouseX <= width && mouseY <= height) { //checks if mouse is in frame
-        
-    let textX = mouseX + 13;
-    let textY = mouseY - 10;
-    let rectX = mouseX + 10;
-    let rectY = mouseY - 22;
-    let textCoordinates = `${round(mouseX)},${round(mouseY)}`;
-    let rectWidth = 7 * textCoordinates.length + 1;
 
-    if(mouseY < 22) {rectY += 31; textY += 31;}
-    if(mouseX > width - 70) {rectX -= 80; textX -= 80;}
+        let textX = mouseX + 13;
+        let textY = mouseY - 10;
+        let rectX = mouseX + 10;
+        let rectY = mouseY - 22;
+        let textCoordinates = `${round(mouseX)},${round(mouseY)}`;
+        let rectWidth = 7 * textCoordinates.length + 1;
 
-    push();
-    cursor('CoordinateHelper/crossHair.cur');
-    fill(0);
-    rect(rectX, rectY, rectWidth, 15, 5);
-    fill(255);
-    text(textCoordinates, textX, textY);
-    pop();
+        if(mouseY < 22) {rectY += 31; textY += 31;}
+        if(mouseX > width - 70) {rectX -= 80; textX -= 80;}
+
+        push();
+        cursor('CoordinateHelper/crossHair.cur');
+        noStroke();
+        fill(0,200);
+        rect(rectX, rectY, rectWidth, 15, 5);
+        fill(255);
+        textSize(12);
+        text(textCoordinates, textX, textY);
+        pop();
     }
 }
